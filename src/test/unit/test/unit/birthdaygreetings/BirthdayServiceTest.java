933e6748339cbd84c4f62fbd847ac8e2d5c37567
Daniel Wellman <dan@danielwellman.com>
2013-06-15 22:55:01 -0400

    Cleaning up tests

--- src/test/unit/test/unit/birthdaygreetings/BirthdayServiceTest.java
+++ src/test/unit/test/unit/birthdaygreetings/BirthdayServiceTest.java
@@ -7,7 +7,7 @@ import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.Random;
+import java.util.Set;
 
 public class BirthdayServiceTest {
     private static final int LEAP_YEAR = 2004;
@@ -16,6 +16,8 @@ public class BirthdayServiceTest {
     private static final MonthAndDay FEB_13 = new MonthAndDay(2, 13);
     private static final MonthAndDay FEB_28 = new MonthAndDay(2, 28);
     private static final MonthAndDay FEB_29 = new MonthAndDay(2, 29);
+
+    private static final Set<Person> SOME_PEOPLE = Sets.hashSet(new Person(new Name("a", "person"), new EmailAddress("some@mail.com"), new Date(1970, 1, 1)));
 
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
@@ -28,8 +30,8 @@ public class BirthdayServiceTest {
     public void notifiesAllPeopleWithBirthdaysOnTheGivenDate() {
         context.checking(new Expectations() {
             {
-                Person person1 = aPersonBornOn(FEB_13.inYear(2001));
-                Person person2 = aPersonBornOn(FEB_13.inYear(2004));
+                Person person1 = new Person(new Name("person", "one"), new EmailAddress("person_1@mail.com"), FEB_13.inYear(2001));
+                Person person2 = new Person(new Name("person", "two"), new EmailAddress("person_2@mail.com"), FEB_13.inYear(2004));
                 allowing(people).withBirthdaysOn(Sets.hashSet(FEB_13)); will(returnValue(Sets.hashSet(person1, person2)));
 
                 oneOf(notifier).notify(person1);
@@ -44,7 +46,7 @@ public class BirthdayServiceTest {
         context.checking(new Expectations() {
             {
                 // Note the allowing from the previous tests is now an expect since the parameters are the important bit
-                oneOf(people).withBirthdaysOn(Sets.hashSet(FEB_28, FEB_29)); will(returnValue(Sets.hashSet(aPerson())));
+                oneOf(people).withBirthdaysOn(Sets.hashSet(FEB_28, FEB_29)); will(returnValue(SOME_PEOPLE));
 
                 ignoring(notifier);
             }
@@ -56,28 +58,12 @@ public class BirthdayServiceTest {
     public void looksOnlyForFeb28BirthdaysInLeapYears() {
         context.checking(new Expectations() {
             {
-                oneOf(people).withBirthdaysOn(Sets.hashSet(FEB_28)); will(returnValue(Sets.hashSet(aPerson())));
+                oneOf(people).withBirthdaysOn(Sets.hashSet(FEB_28)); will(returnValue(SOME_PEOPLE));
 
                 ignoring(notifier);
             }
         });
         birthdayService.sendGreetings(FEB_28.inYear(LEAP_YEAR));
-    }
-
-    private static int uniqueCounter = 0;
-
-    private Person aPersonBornOn(Date birthdate) {
-        int uniqueSuffix = uniqueCounter++;
-        return new Person(new Name("firstName", "lastName" + uniqueSuffix), new EmailAddress("whatever" + uniqueSuffix + "@foo.com"), birthdate);
-    }
-
-    private Person aPerson() {
-        int uniqueSuffix = uniqueCounter++;
-        Random randomGenerator = new Random();
-        int randomMonth = randomGenerator.nextInt(11) + 1;
-        int randomDay = randomGenerator.nextInt(27) + 1;
-        MonthAndDay birthDay = new MonthAndDay(randomMonth, randomDay);
-        return new Person(new Name("firstName", "lastName" + uniqueSuffix), new EmailAddress("whatever" + uniqueSuffix + "@foo.com"), birthDay.inYear(2013));
     }
 
 }
