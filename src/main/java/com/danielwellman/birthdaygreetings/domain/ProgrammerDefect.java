af36195737ea59563e8c510ef603d3186140b55b
Daniel Wellman <dan@danielwellman.com>
2013-06-15 23:00:00 -0400

    Deleting unused class

--- src/main/java/com/danielwellman/birthdaygreetings/domain/ProgrammerDefect.java
+++
@@ -1,7 +0,0 @@
-package com.danielwellman.birthdaygreetings.domain;
-
-public class ProgrammerDefect extends RuntimeException {
-    public ProgrammerDefect(String message) {
-        super(message);
-    }
-}
